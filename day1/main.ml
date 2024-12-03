478615cc939aae678b5a1935e649321930c61b84
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2024-12-03 01:43:11 +0100

    day1.2

--- day1/main.ml
+++ day1/main.ml
@@ -21,13 +21,39 @@ let distance : int -> int -> int =
   let diff = a - b in
   if diff < 0 then -diff else diff
 
-let distance_lists : int list * int list -> int =
- fun (xs, ys) -> List.fold_left2 (fun acc x y -> acc + distance x y) 0 xs ys
+let distance_lists : int list -> int list -> int =
+ fun xs ys -> List.fold_left2 (fun acc x y -> acc + distance x y) 0 xs ys
+
+module IntMap = Map.Make (Int)
+
+let add_int_to_map : int IntMap.t -> int -> int IntMap.t =
+ fun map x ->
+  IntMap.update x
+    (fun option -> Option.some @@ (Option.value option ~default:0 + 1))
+    map
+
+let count_numbers : int list -> int IntMap.t =
+ fun ints -> List.fold_left add_int_to_map IntMap.empty ints
+
+let single_similarity : int IntMap.t -> int -> int -> int =
+ fun map acc x ->
+  Option.value
+    (Option.map (fun count -> count * x) (IntMap.find_opt x map))
+    ~default:0
+  + acc
+
+let similarity : int list -> int list -> int =
+ fun xs ys ->
+  let number_counts = count_numbers ys in
+  List.fold_left (single_similarity number_counts) 0 xs
 
 let () =
-  let distance =
+  let xs, ys =
     read_lines "day1/input.txt"
     |> List.map parse_line |> list_of_tuple_to_tuple_of_lists
-    |> sort_tuple_of_lists |> distance_lists
+    |> sort_tuple_of_lists
   in
-  print_endline @@ string_of_int distance
+  let distance = distance_lists xs ys in
+  let similarity = similarity xs ys in
+  print_endline @@ string_of_int distance;
+  print_endline @@ string_of_int similarity
