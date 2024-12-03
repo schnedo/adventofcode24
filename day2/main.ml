0c9af3e460d83a8aff510457df8413b9a9b649f3
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2024-12-03 22:51:42 +0100

    not quite complete day2.2

--- day2/main.ml
+++ day2/main.ml
@@ -1,7 +1,17 @@
+(*let test_input =*)
+(*  "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"*)
+(**)
+(*let record_lines = String.split_on_char '\n' test_input*)
+
 let read_lines : string -> string list =
  fun file -> In_channel.with_open_bin file In_channel.input_lines
 
 let record_lines = read_lines "day2/input.txt"
+
+let print_record =
+  List.iter (fun n ->
+      print_int n;
+      print_char ' ')
 
 let record_line_to_record (line : string) =
   List.map int_of_string @@ String.split_on_char ' ' line
@@ -9,12 +19,15 @@ let record_line_to_record (line : string) =
 let safe_increasing (diff : int) = diff > 0 && diff < 4
 let safe_decreasing (diff : int) = diff < 0 && diff > -4
 
-let rec is (safe_direction : int -> bool) (record : int list) =
+let rec is (safe_direction : int -> bool) ?(try_again = true)
+    (record : int list) =
   match record with
-  | first :: second :: tail ->
+  | first :: second :: tail -> (
       let diff = second - first in
-      if not @@ safe_direction diff then false
-      else is safe_direction @@ (second :: tail)
+      match (safe_direction diff, try_again) with
+      | true, _ -> is safe_direction ~try_again (second :: tail)
+      | false, true -> is safe_direction ~try_again:false (first :: tail)
+      | false, false -> false)
   | _ -> true
 
 let is_safe (record : int list) =
@@ -22,4 +35,18 @@ let is_safe (record : int list) =
 
 let records = List.map record_line_to_record record_lines
 let safe_records = List.filter is_safe records
-let () = string_of_int @@ List.length safe_records |> print_endline
+
+let () =
+  List.iter
+    (fun record ->
+      print_record record;
+      print_newline ())
+    records;
+  print_newline ();
+  List.iter
+    (fun record ->
+      print_record record;
+      print_newline ())
+    safe_records;
+  print_newline ();
+  string_of_int @@ List.length safe_records |> print_endline
